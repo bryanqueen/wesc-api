d60d1af066947774d8ba68170779336481a4334a
bryanqueen <somtochukwuumehdaniel@gmail.com>
2024-02-14 22:51:18 +0100

    Testing the waters, Might revert back

--- server.js
+++ server.js
@@ -45,8 +45,10 @@ app.use(`${path}jobs`, jobsbannerRoutes)
 
 
 
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 
-app.listen(port, ()=>{
-    console.log(`Server is running on localhost:${port}`)
-})
+// app.listen(port, ()=>{
+//     console.log(`Server is running on localhost:${port}`)
+// })
+
+module.exports = app;
