0a8ba735e54c4d04c9ea52908c35bbc61e9dd847
bryanqueen <somtochukwuumehdaniel@gmail.com>
2024-02-18 18:02:48 +0100

    Changed the body field type to string

--- models/Blog.js
+++ models/Blog.js
@@ -11,7 +11,7 @@ const blogSchema = new mongoose.Schema({
         required: true
     },
     body: {
-        type: Array,
+        type: String,
         required: true,
     },
     comments: [{
